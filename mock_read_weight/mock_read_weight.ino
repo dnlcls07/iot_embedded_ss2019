4cac339d961b45c3b6f7937b7ca42abb8830473a
miguelgrivera <ie698588@iteso.mx>
2019-05-01 10:48:21 -0500

    Add weigth sensor example code

--- mock_read_weight/mock_read_weight.ino
+++ mock_read_weight/mock_read_weight.ino
@@ -1,13 +1,13 @@
 #include <HX711_ADC.h> // https://github.com/olkal/HX711_ADC
 #include <Wire.h>
 
-HX711_ADC LoadCell(D2, D3); // parameters: dt pin, sck pin<span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span>
+HX711_ADC LoadCell(9, 8); // parameters: dt pin, sck pin<span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span>
 
 void setup() {
   Serial.begin(9600);
   LoadCell.begin(); // start connection to HX711
   LoadCell.start(2000); // load cells gets 2000ms of time to stabilize
-  LoadCell.setCalFactor(999.0); // calibration factor for load cell => strongly dependent on your individual setup
+  LoadCell.setCalFactor(1); // calibration factor for load cell => strongly dependent on your individual setup
 }
 
 void loop() {
